c71b275154f8877b7ecb405db367e96f62d9713d
andreluanc <38443963+andreluanc@users.noreply.github.com>
2018-04-16 20:16:20 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pss;
+
+/**
+ *
+ * @author guest-afjrfe
+ */
+public abstract class Pessoa{
+    String nome;
+    int cpf;
+    String telefone;
+    String endereco;
+}
